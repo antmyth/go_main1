cb7886d5229412188b0c76994cf97ee8ee3a306f
Antonio Pedro Nascimento <antonio.p.nascimento@gmail.com>
2015-09-23 17:36:40 +0100

    update15

--- src/main/scala/com/nature/hub/gotest/main/one.scala
+++ src/main/scala/com/nature/hub/gotest/main/one.scala
@@ -3,6 +3,6 @@ package com.nature.hub.gotest.main
 import com.nature.hub.gotest.common.SomeCommonClass
 
 class One {
-  //some comment updated again
+  //some comment updated again update15
   def useTheCommon(s:String) = new SomeCommonClass().someCommonFunction(s)
 }
