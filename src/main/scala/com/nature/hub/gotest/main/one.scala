4f002743d1e61928ff5792d56c582ef1cf126832
Antonio Pedro Nascimento <antonio.p.nascimento@gmail.com>
2015-09-23 17:17:15 +0100

    update 10

--- src/main/scala/com/nature/hub/gotest/main/one.scala
+++ src/main/scala/com/nature/hub/gotest/main/one.scala
@@ -3,5 +3,6 @@ package com.nature.hub.gotest.main
 import com.nature.hub.gotest.common.SomeCommonClass
 
 class One {
+  //some comment
   def useTheCommon(s:String) = new SomeCommonClass().someCommonFunction(s)
 }
